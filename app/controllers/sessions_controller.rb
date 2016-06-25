2aa9073375ee2cc906116c08fe0b7f55371bb29a
Elliot Wolfe <email": null>
2016-06-25 14:33:53 -0700

    Finish basic user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
-
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      session[:user_id]= @user.id
+      redirect_to bands_path
+    else
+      @errors = []
+      @errors << "Credentials don't match our records."
+      render "new"
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, password)
     user = User.find_by(email: email)
-    if user && user.password = password
+    if user && user.password == password
       user
     else
       nil
