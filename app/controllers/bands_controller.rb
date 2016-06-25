211e64963c50887787f2e54f969d2ede6ef84665
Elliot Wolfe <email": null>
2016-06-25 15:18:39 -0700

    Added Create and Read functions for Band

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,29 @@
+class BandsController < ApplicationController
+
+  def index
+    @bands = Band.all
+  end
+
+  def new
+    @band= Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to band_path(@band)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625200602) do
+ActiveRecord::Schema.define(version: 20160625214640) do
+
+  create_table "bands", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
