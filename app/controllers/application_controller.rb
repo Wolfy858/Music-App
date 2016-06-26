2d545ddbea75fc4aa300889d7a5b94121952ab35
Elliot Wolfe <email": null>
2016-06-25 19:25:58 -0700

    Added additional links for navigation between resources. Added before filters to prevent users who are not logged in from accessing restricted pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged to access this section"
+      redirect_to login_path
+    end
+  end
 end
